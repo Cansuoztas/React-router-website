40426b6160e16f525a265b829fd653c059bfc591
Cansu Öztaş <fcansuoztas@gmail.com>
2022-10-30 23:36:19 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ import Html from "./pages/Html"
 import Css from "./pages/Css";
 import Logo from "./pages/Logo";
 
+
 function App() {
   return (
     <>
       <Navbar />
-     <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path ="/about" element={<About/>}/>
-        <Route path="/services" element={<Services/>}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
         <Route path="/html" element={<Html />} />
         <Route path="/css" element={<Css />} />
         <Route path="/logo" element={<Logo />} />
-     </Routes>
+      </Routes>
+    
       
       <Footer />
     </>
